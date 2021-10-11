7e5c796846ed148f683a4beb1c92bac234a7b4da
Tintenpatron <p429344@gmail.com>
2021-10-11 12:03:05 +0200

    :wrench: added math utils

---
+++ src/helper/mathUtil.js
@@ -0,0 +1,61 @@
+class MathUtil {
+
+    /**
+     * Check if a is lower than b
+     */
+    static isLowerThan(a, b){
+        return a < b;
+    }
+
+    /**
+     * Check if a is greater than b
+     */
+    static isGreaterThan(a, b){
+        return a > b;
+    }
+
+    /**
+     * Check if c is between a and b
+     */
+    static isBetween(c, a, b) {
+        return c >= a && c <= b;
+    }
+
+    /**
+     * Check if a is negative
+     */
+    static isNegative(a){
+        return a < 0;
+    }
+
+    /**
+     * Check if a is positive
+     */
+    static isPositive(a){
+        return a > 0;
+    }
+
+    /**
+     * Check if a is 0
+     */
+    static isZero(a){
+        return a === 0;
+    }
+
+    /**
+     * Check if a is even
+     */
+    static isEven(a){
+        return a % 2 === 0;
+    }
+
+    /**
+     * Check if a is odd
+     */
+    static isOdd(a){
+        return a % 2 !== 0;
+    }
+
+}
+
+module.exports = MathUtil;
