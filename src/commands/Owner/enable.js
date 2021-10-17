ae4e4402ed409c1d575159c9fa25f93b070b0029
Tintenpatron <p429344@gmail.com>
2021-10-17 20:52:39 +0200

    :rocket: improved enable command

--- src/commands/Owner/enable.js
+++ src/commands/Owner/enable.js
@@ -1,6 +1,6 @@
 const Command = require('../../structure/Command')
     , { MessageEmbed } = require('discord.js')
-    , Discord = require('discord.js');
+	, fs = require('fs');
 
 class Enable extends Command {
     constructor(client) {
@@ -8,69 +8,74 @@ class Enable extends Command {
             name: "enable",
             description: "owner/enable:description",
             dirname: __dirname,
-            memberPermissions: ["SEND_MESSAGES"],
-            botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
-            ownerOnly: false,
-            staffOnly: true,
+            ownerOnly: true,
             cooldown: 20000,
             slashCommand: {
-                addCommand: true,
-				options: [
-                    {
-                        name: "owner/enable:slashOption1",
-                        description: "owner/enable:slashOption1Desc",
-                        type: "STRING",
-                        required: true
-                    },
-
-
-                ]
+                addCommand: false
             }
         });
     }
     async run(interaction, message, args, data) {
-		const fs = require('fs')
-        const command = args[0];
-		const guild = interaction?.guild || message?.guild
-            , member = interaction?.member || message?.member
-            , channel = interaction?.channel || message?.channel;
-		const cmd = this.client.commands.get(command) || this.client.commands.get(this.client.aliases.get(command));
+		const guild = interaction?.guild || message?.guild;
+
+		if(!args[0]){
+			let embed = new MessageEmbed()
+				.setAuthor(this.client.user.username, this.client.user.displayAvatarURL(), this.client.website)
+				.setDescription(guild.translate("owner/enable:usage")
+						.replace('{prefix}', data.guild.prefix)
+						.replace('{emotes.use}', this.client.emotes.use) + '\n' +
+					guild.translate("owner/enable:example")
+						.replace('{prefix}', data.guild.prefix)
+						.replace('{emotes.example}', this.client.emotes.example))
+				.setColor(this.client.embedColor)
+				.setFooter(data.guild.footer);
+			if(message) return message.send(embed);
+			if(interaction) return interaction.send(embed);
+		}
+		const command = args[0]
+			, cmd = this.client.commands.get(command) || this.client.commands.get(this.client.aliases.get(command));
+
 		if(!cmd){
 			let embed = new MessageEmbed()
 				.setAuthor(this.client.user.username, this.client.user.displayAvatarURL(), this.client.website)
 				.setDescription(guild.translate("owner/enable:usage")
-					.replace('{emotes.use}', this.client.emotes.use)
-					.replace('{prefix}', data.guild.prefix))
+						.replace('{prefix}', data.guild.prefix)
+						.replace('{emotes.use}', this.client.emotes.use) + '\n' +
+					guild.translate("owner/enable:example")
+						.replace('{prefix}', data.guild.prefix)
+						.replace('{emotes.example}', this.client.emotes.example))
 				.setColor(this.client.embedColor)
 				.setFooter(data.guild.footer);
-			if (interaction) return interaction.send(embed);
-			if (message) return message.send(embed, false)
+			if(message) return message.send(embed);
+			if(interaction) return interaction.send(embed);
 		}
 
-		let disablestate = false;
-		let file = JSON.parse(fs.readFileSync("storage/disabledcmds.json"));
+		let disabled
+			, file = JSON.parse(fs.readFileSync('storage/disabledcmds.json'));
+
 		for(let attributename in file){
 			if(file[attributename].toLowerCase() === cmd.help.name) {
-				disablestate = true;
+				disabled = true;
 			}
 		}
 
-		if(!disablestate) {
+		if(!disabled) {
 			let embed = new MessageEmbed()
 				.setAuthor(this.client.user.username, this.client.user.displayAvatarURL(), this.client.website)
 				.setDescription(guild.translate("owner/enable:isNot")
 					.replace('{emotes.error}', this.client.emotes.use)
-					.replace('{cmd}', cmd.help.name))
+					.replace('{cmd}', cmd.help.name.toString().charAt(0).toUpperCase() + cmd.help.name.toString().slice(1)))
 				.setColor(this.client.embedColor)
 				.setFooter(data.guild.footer);
 			if(interaction) return interaction.send(embed);
 			if(message) return message.send(embed, false)
 		}
 
-		const removeData = cmd.help.name;
-		const dataa = fs.readFileSync('storage/disabledcmds.json');
-		let json = JSON.parse(dataa);
-		json = json.filter((name) => { return name !== cmd.help.name });
+		let remove = cmd.help.name
+			, jsonData = fs.readFileSync('storage/disabledcmds.json')
+			,json = JSON.parse(jsonData);
+
+		json = json.filter((value) => value !== cmd.help.name);
 		fs.writeFileSync('storage/disabledcmds.json', JSON.stringify(json, null, 2));
 
 
@@ -78,7 +83,7 @@ class Enable extends Command {
 			.setAuthor(this.client.user.username, this.client.user.displayAvatarURL(), this.client.website)
 			.setDescription(guild.translate("owner/enable:enabled")
 				.replace('{emotes.success}', this.client.emotes.success)
-				.replace('{cmd}', cmd.help.name))
+				.replace('{cmd}', cmd.help.name.toString().charAt(0).toUpperCase() + cmd.help.name.toString().slice(1)))
 			.setColor(this.client.embedColor)
 			.setFooter(data.guild.footer);
 		if(interaction) return interaction.send(embed);
