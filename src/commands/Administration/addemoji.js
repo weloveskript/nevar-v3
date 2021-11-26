260a0f3c094751ef32b0cf11af0468711a97cd48
Tintenpatron <p429344@gmail.com>
2021-11-27 00:24:37 +0100

    :zap: improved addemoji command

--- src/commands/Administration/addemoji.js
+++ src/commands/Administration/addemoji.js
@@ -63,8 +63,9 @@ class Addemoji extends Command {
             return (res !== null)
         }
 
+        let emote;
         if(!isUrl(arg)){
-            let emote = Discord.Util.parseEmoji(args[0]);
+            emote = Discord.Util.parseEmoji(args[0]);
             if(emote){
                 arg = `https://cdn.discordapp.com/emojis/${emote.id}.${emote.animated ? 'gif' : 'png'}`;
             }
@@ -72,65 +73,97 @@ class Addemoji extends Command {
 
         let name = args[1] ? args[1].replace(/[^a-z0-9]/gi, "") : null;
         if(!name){
-            let embed = new MessageEmbed()
-                .setAuthor(this.client.user.username, this.client.user.displayAvatarURL(), this.client.website)
-                .setDescription(guild.translate("administration/addemoji:usage")
-                        .replace('{prefix}', data.guild.prefix)
-                        .replace('{emotes.use}', this.client.emotes.use) + '\n' +
-                    guild.translate("administration/addemoji:example")
-                        .replace('{prefix}', data.guild.prefix)
-                        .replace('{emotes.example}', this.client.emotes.example))
-                .setColor(this.client.embedColor)
-                .setFooter(data.guild.footer);
-            if(message) return message.send(embed);
-            if(interaction) return interaction.send(embed);
-        }
-
-        if(name.length < 2 || name.length > 32){
-            let embed = new MessageEmbed()
-                .setAuthor(this.client.user.username, this.client.user.displayAvatarURL(), this.client.website)
-                .setDescription(guild.translate("administration/addemoji:length")
-                    .replace('{emotes.error}', this.client.emotes.error))
-                .setColor(this.client.embedColor)
-                .setFooter(data.guild.footer);
-            if(message) return message.send(embed);
-            if(interaction) return interaction.send(embed, true);
-        }
-
-        guild.emojis
-            .create(arg, name)
-            .then(emoji => {
+            console.log(emote)
+            if(emote){
+                name = emote.name;
+                guild.emojis
+                    .create(arg, emote.name)
+                    .then(emoji => {
+                        let embed = new MessageEmbed()
+                            .setAuthor(this.client.user.username, this.client.user.displayAvatarURL(), this.client.website)
+                            .setDescription(guild.translate("administration/addemoji:created")
+                                .replace('{emotes.success}', this.client.emotes.success)
+                                .replace('{name}', (emoji.animated ? 'a:'+emoji.name : ':'+emoji.name))
+                                .replace('{id}', emoji.id))
+                            .setColor(this.client.embedColor)
+                            .setFooter(data.guild.footer);
+                        if(message) return message.send(embed);
+                        if(interaction) return interaction.send(embed);
+                    })
+                    .catch(async (error) => {
+                        let embed = new MessageEmbed()
+                            .setAuthor(this.client.user.username, this.client.user.displayAvatarURL(), this.client.website)
+                            .setDescription(guild.translate("administration/addemoji:cantCreate")
+                                .replace('{emotes.error}', this.client.emotes.error)
+                                .replace('{emotes.arrow}', this.client.emotes.arrow)
+                                .replace('{emotes.arrow}', this.client.emotes.arrow)
+                                .replace('{support}', this.client.supportUrl))
+                            .setColor(this.client.embedColor)
+                            .setFooter(data.guild.footer);
+                        if(message) message.send(embed);
+                        if(interaction) interaction.send(embed, true)
+                        let user = message?.author || interaction?.member.user
+                        let type = 'Message-Command'
+                        if(interaction) {
+                            type = 'Slash-Command'
+                        }
+                        this.client.logError(error, user, guild, `${message ? data.guild.prefix : '/'}addemoji ${args[0]} ${args[1]}`, type)
+                    })
+            }else{
                 let embed = new MessageEmbed()
                     .setAuthor(this.client.user.username, this.client.user.displayAvatarURL(), this.client.website)
-                    .setDescription(guild.translate("administration/addemoji:created")
-                        .replace('{emotes.success}', this.client.emotes.success)
-                        .replace('{name}', (emoji.animated ? 'a:'+emoji.name : ':'+emoji.name))
-                        .replace('{id}', emoji.id))
+                    .setDescription(guild.translate("administration/addemoji:chooseName")
+                            .replace('{emotes.error}', this.client.emotes.error))
                     .setColor(this.client.embedColor)
                     .setFooter(data.guild.footer);
                 if(message) return message.send(embed);
                 if(interaction) return interaction.send(embed);
-            })
-            .catch(async (error) => {
+            }
+        }else{
+            if(name.length < 2 || name.length > 32){
                 let embed = new MessageEmbed()
                     .setAuthor(this.client.user.username, this.client.user.displayAvatarURL(), this.client.website)
-                    .setDescription(guild.translate("administration/addemoji:cantCreate")
-                        .replace('{emotes.error}', this.client.emotes.error)
-                        .replace('{emotes.arrow}', this.client.emotes.arrow)
-                        .replace('{emotes.arrow}', this.client.emotes.arrow)
-                        .replace('{support}', this.client.supportUrl))
+                    .setDescription(guild.translate("administration/addemoji:length")
+                        .replace('{emotes.error}', this.client.emotes.error))
                     .setColor(this.client.embedColor)
                     .setFooter(data.guild.footer);
-                if(message) message.send(embed);
-                if(interaction) interaction.send(embed, true)
-                let user = message?.author || interaction?.member.user
-                let type = 'Message-Command'
-                if(interaction) {
-                    type = 'Slash-Command'
-                }
-                this.client.logError(error, user, guild, `${message ? data.guild.prefix : '/'}addemoji ${args[0]} ${args[1]}`, type)
-
-            })
+                if(message) return message.send(embed);
+                if(interaction) return interaction.send(embed, true);
+            }
+            guild.emojis
+                .create(arg, name)
+                .then(emoji => {
+                    let embed = new MessageEmbed()
+                        .setAuthor(this.client.user.username, this.client.user.displayAvatarURL(), this.client.website)
+                        .setDescription(guild.translate("administration/addemoji:created")
+                            .replace('{emotes.success}', this.client.emotes.success)
+                            .replace('{name}', (emoji.animated ? 'a:'+emoji.name : ':'+emoji.name))
+                            .replace('{id}', emoji.id))
+                        .setColor(this.client.embedColor)
+                        .setFooter(data.guild.footer);
+                    if(message) return message.send(embed);
+                    if(interaction) return interaction.send(embed);
+                })
+                .catch(async (error) => {
+                    let embed = new MessageEmbed()
+                        .setAuthor(this.client.user.username, this.client.user.displayAvatarURL(), this.client.website)
+                        .setDescription(guild.translate("administration/addemoji:cantCreate")
+                            .replace('{emotes.error}', this.client.emotes.error)
+                            .replace('{emotes.arrow}', this.client.emotes.arrow)
+                            .replace('{emotes.arrow}', this.client.emotes.arrow)
+                            .replace('{support}', this.client.supportUrl))
+                        .setColor(this.client.embedColor)
+                        .setFooter(data.guild.footer);
+                    if(message) message.send(embed);
+                    if(interaction) interaction.send(embed, true)
+                    let user = message?.author || interaction?.member.user
+                    let type = 'Message-Command'
+                    if(interaction) {
+                        type = 'Slash-Command'
+                    }
+                    this.client.logError(error, user, guild, `${message ? data.guild.prefix : '/'}addemoji ${args[0]} ${args[1]}`, type)
+                })
+        }
     }
 }
 
