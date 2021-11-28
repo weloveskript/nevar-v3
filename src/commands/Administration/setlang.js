7c93d164ef43a03a02a3a70846dcd46de4b480b5
Tintenpatron <p429344@gmail.com>
2021-11-28 21:34:52 +0100

    :rocket: added setlang command

---
+++ src/commands/Administration/setlang.js
@@ -0,0 +1,82 @@
+const Command = require('../../core/command')
+    , { MessageEmbed, MessageActionRow, MessageButton} = require('discord.js');
+
+class Setlang extends Command {
+
+    constructor(client) {
+        super(client, {
+            name: "setlang",
+            description: "administration/setlang:description",
+            dirname: __dirname,
+            aliases: ["lang"],
+            memberPermissions: ["MANAGE_GUILD"],
+            cooldown: 5000,
+            slashCommand: {
+                addCommand: true
+            }
+        });
+    }
+
+    async run(interaction, message, args, data){
+        let guild = message?.guild || interaction?.guild;
+
+        let embed = new MessageEmbed()
+            .setAuthor(this.client.user.username, this.client.user.displayAvatarURL(), this.client.website)
+            .setDescription(guild.translate("administration/setlang:chooseLang")
+                .replace('{emotes.arrow}', this.client.emotes.arrow))
+            .setColor(this.client.embedColor)
+            .setFooter(data.guild.footer);
+
+        const { MessageButton, MessageActionRow } = require('discord.js');
+        let id = message?.member?.user?.id || interaction?.member?.user?.id
+        let row = new MessageActionRow();
+        for(let lang of this.client.languages){
+            let item = new MessageButton()
+                .setCustomId('lang_'+ id + '_' + lang.name)
+                .setLabel(lang.nativeName)
+                .setStyle('PRIMARY')
+                .setEmoji(lang.emoji);
+           if(lang.name === data.guild.language){
+               item.setStyle('SECONDARY')
+              item.setDisabled(true);
+           }
+           row.components.push(item);
+        }
+
+        let sent;
+        if (message) sent = await message.send(embed, false, [row]);
+        if (interaction) sent = await interaction.send(embed, false, [row]);
+
+        const filter = i => i.customId.startsWith('lang_'+ id) && i.user.id === id;
+
+        const clicked = await sent.awaitMessageComponent({ filter, time: 60000 }).catch(() => {})
+
+        if(clicked){
+            let lang = this.client.languages.find((l) => l.name === clicked.customId.toString().split('_')[2]);
+            if(!lang){
+                let embed = new MessageEmbed()
+                    .setAuthor(this.client.user.username, this.client.user.displayAvatarURL(), this.client.website)
+                    .setDescription(guild.translate("language:error")
+                        .replace('{emotes.error}', this.client.emotes.error))
+                    .setColor(this.client.embedColor)
+                    .setFooter(data.guild.footer);
+                return sent.edit({embeds:[embed], components: []});
+            }else{
+                data.guild.language = lang.name;
+                await data.guild.save();
+                let embed = new MessageEmbed()
+                    .setAuthor(this.client.user.username, this.client.user.displayAvatarURL(), this.client.website)
+                    .setDescription(guild.translate("language:set")
+                        .replace('{emotes.error}', this.client.emotes.error))
+                    .setColor(this.client.embedColor)
+                    .setFooter(data.guild.footer);
+                return sent.edit({embeds:[embed], components: []});
+
+            }
+        }
+
+    }
+
+}
+
+module.exports = Setlang;
