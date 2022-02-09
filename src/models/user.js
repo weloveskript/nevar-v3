f858697a9e6f0837b3e15183a2985d84e00c967c
Tintenpatron <p429344@gmail.com>
2022-02-09 20:09:29 +0100

    :heavy_plus_sign: mylevelsettings

--- src/models/user.js
+++ src/models/user.js
@@ -2,18 +2,35 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
 
-    id: { type: String },
+    id: {
+        type: String
+    },
 
-    levelColor: { type: String, default: "5773c9"},
-    levelBackground: { type: Number, default: 0 },
+    levelColor: {
+        type: String,
+        default: "5773c9"
+    },
+    levelBackground: {
+        type: Number,
+        default: 0
+    },
 
-    registeredAt: { type: Number, default: Date.now() },
+    registeredAt: {
+        type: Number,
+        default: Date.now()
+    },
 
-    cooldowns: { type: Object, default: {
+    cooldowns: {
+        type: Object,
+        default: {
             rep: 0
-        }},
+        }
+    },
 
-    blocked: { type: Boolean, default: false }
+    blocked: {
+        type: Boolean,
+        default: false
+    }
 
 });
 
