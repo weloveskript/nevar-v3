57ed3c6761dcc91bb8ac5b8c42fc66284be25b1c
Tintenpatron <p429344@gmail.com>
2022-03-15 21:08:49 +0100

    :rocket: improved level command

--- src/commands/misc/level.js
+++ src/commands/misc/level.js
@@ -4,7 +4,6 @@ const Canvas = require('canvas');
 const Discord = require('discord.js');
 const { CanvasRenderingContext2D } = require('canvas');
 const {SlashCommandBuilder} = require("@discordjs/builders");
-const Resolver = require('../../helper/resolver');
 Canvas.registerFont('./storage/fonts/KeepCalm-Medium.ttf', { family: 'KeepCalm' });
 
 
@@ -55,7 +54,7 @@ class Level extends Command {
     }
     async run(interaction, message, args, data){
 
-        // Get the user, channel, and guild
+        // Get all needed data
         const channel = interaction?.channel || message?.channel;
         const guild = interaction?.guild || message?.guild;
         let member = interaction?.member || message?.member;
@@ -71,13 +70,18 @@ class Level extends Command {
             level: await Levels.fetch(member.user.id, guild.id, true)
         };
 
-        //Create image
+        // create image
         const canvas = Canvas.createCanvas(1000, 300);
         const ctx = canvas.getContext('2d');
 
+        // quality settings
+        ctx.patternQuality = "best";
+        ctx.antialias = "default";
+        ctx.filter = "default";
+
         // draw gray background
-        ctx.fillStyle = "#23272a";
-        ctx.roundRect(0, 0, canvas.width, canvas.height, 8).fill();
+        ctx.fillStyle = "#1D2228";
+        ctx.roundRect(0, 0, canvas.width, canvas.height, 22).fill();
 
         // draw black square if no custom image is chosen
         if(user.db.levelBackground === 0){
@@ -92,82 +96,49 @@ class Level extends Command {
             ctx.clip();
             ctx.drawImage(background, 25, 25, 950, 250);
             ctx.restore();
+
+            ctx.strokeStyle = '#1D2228';
+            ctx.lineWidth = 3.5;
+            ctx.roundRect(25, 25, 950, 250, 10).stroke();
         }
 
         // write username
-        ctx.fillStyle = "#ffffff"; // white text
-        let text = user.discord.user.username; // text to draw
-        if(text.length > 15) text = text.substring(0, 15); // cut text if too long
-        ctx.font = "60px Calibri" // font size and type
-        ctx.fillText(text, 290, 170) // draw text
+        ctx.fillStyle = "#ffffff";
+        let text = user.discord.user.username;
+        ctx.font = "60px Calibri"
+        let size = 60;
+
+        // loop until the text fits within the width of the box
+        for(let i = 6; i <= 32; i++){
+            size = size-1.36;
+            if(text.length === i) ctx.font = size + "px Calibri";
+        }
+        ctx.fillText(text, 290, 170)
 
         // write level and rank
-        ctx.fillStyle = "#ffffff";
         text = user.level.position;
-        ctx.font = "61px Calibri"
-        if(text.toString().length === 1) {
-            ctx.fillText("#" + text, 605, 95)
-            let rankText = guild.translate("misc/level:main:rank");
-            ctx.font = "36px Calibri";
-            ctx.fillText(rankText, 520, 95)
+        let rankText1X = 595;
+        let rankText2X = 675;
+        let rankText = guild.translate("misc/level:main:rank");
 
-            ctx.fillStyle = "#"+user.db.levelColor;
-            let levelText = guild.translate("misc/level:main:level");
-            ctx.font = "36px Calibri"
-            ctx.fillText(levelText, 730, 95)
-            ctx.fillStyle = "#"+user.db.levelColor;
-            let levelIntText = user.level.level;
-            ctx.font = "61px Calibri"
-            ctx.fillText(levelIntText, 815, 95)
+        // loop until the text fits within the width of the box
+        for(let i = 1; i <= 7; i++){
+            rankText1X = rankText1X-25.5;
+            rankText2X = rankText2X-25.5;
+            if(text.toString().length === i){
+                ctx.font = "36px Calibri";
+                ctx.fillStyle = "#ffffff";
+                ctx.fillText(rankText, rankText1X, 95);
+                ctx.font = "61px Calibri";
+                ctx.fillText('#' + text, rankText2X, 95);
+            }
         }
-        if(text.toString().length === 2) {
-            ctx.fillText("#" + text, 590, 95)
-            ctx.fillStyle = "#ffffff";
-            let rankText = guild.translate("misc/level:main:rank");
-            ctx.font = "36px Calibri";
-            ctx.fillText(rankText, 505, 95)
-
-            ctx.fillStyle = "#"+user.db.levelColor;
-            let levelText = guild.translate("misc/level:main:level");
-            ctx.font = "36px Calibri"
-            ctx.fillText(levelText, 745, 95)
-            ctx.fillStyle = "#"+user.db.levelColor;
-            let levelIntText = user.level.level;
-            ctx.font = "61px Calibri"
-            ctx.fillText(levelIntText, 830, 95)
-        }
-        if(text.toString().length === 3) {
-            ctx.fillText("#" + text, 575, 95)
-            ctx.fillStyle = "#ffffff";
-            let rankText = guild.translate("misc/level:main:rank");
-            ctx.font = "36px Calibri";
-            ctx.fillText(rankText, 490, 95)
-
-            ctx.fillStyle = "#"+user.db.levelColor;
-            let levelText = guild.translate("misc/level:main:level");
-            ctx.font = "36px Calibri"
-            ctx.fillText(levelText, 745, 95)
-            ctx.fillStyle = "#"+user.db.levelColor;
-            let levelIntText = user.level.level;
-            ctx.font = "61px Calibri"
-            ctx.fillText(levelIntText, 830, 95)
-        }
-        if(text.toString().length === 4) {
-            ctx.fillText("#" + text, 545, 95)
-            ctx.fillStyle = "#ffffff";
-            let rankText = guild.translate("misc/level:main:rank");
-            ctx.font = "36px Calibri";
-            ctx.fillText(rankText, 460, 95)
-
-            ctx.fillStyle = "#"+user.db.levelColor;
-            let levelText = guild.translate("misc/level:main:level");
-            ctx.font = "36px Calibri"
-            ctx.fillText(levelText, 745, 95)
-            ctx.fillStyle = "#"+user.db.levelColor;
-            let levelIntText = user.level.level;
-            ctx.font = "61px Calibri"
-            ctx.fillText(levelIntText, 830, 95)
-        }
+        // write level
+        ctx.fillStyle = '#' + user.db.levelColor;
+        ctx.font = "36px Calibri";
+        ctx.fillText(guild.translate("misc/level:main:level"), 740, 95);
+        ctx.font = "61px Calibri";
+        ctx.fillText(user.level.level, 830, 95);
 
         // write current xp / needed xp
         function kFormatter(int){
@@ -178,25 +149,26 @@ class Level extends Command {
         text = kFormatter(user.level.xp) + ' / ' + kFormatter(Levels.xpFor(user.level.level + 1)) + ' XP';
         ctx.font = "29px Calibri";
 
-        // draw text
-        if(text.length === 10) ctx.fillText(text, 740, 170);
-        if(text.length > 10 && text.length <= 13) ctx.fillText(text, 710, 170);
-        if(text.length > 13 && text.length < 15) ctx.fillText(text, 695, 170);
-        if(text.length > 15 && text.length < 17) ctx.fillText(text, 675, 170);
-        if(text.length > 17 && text.length < 21) ctx.fillText(text, 660, 170);
-        if(text.length > 21 && text.length < 25) ctx.fillText(text, 615, 170);
-        if(text.length > 25 && text.length < 29) ctx.fillText(text, 590, 170);
+        let X = 763;
+        // loop until the text fits within the width of the box
+        for(let i = 10; i <= 30; i++){
+            if(text.length === i){
+                ctx.fillText(text, X, 170)
+            }
+            X = X - 15;
+        }
 
         // draw progress bar
-        ctx.roundRect(290, 185, 600, 40, 13).fill();
+        ctx.roundRect(290, 185, 600, 40, 25).fill();
 
         // draw progress bar fill
         const xpPercent = (user.level.xp * 100) / Levels.xpFor(user.level.level + 1);
-        const percentWidth = (xpPercent * 600) / 100;
+        const neededWidth = (xpPercent * 600) / 100;
         ctx.fillStyle = '#'+user.db.levelColor;
-        ctx.roundRect(290, 185, percentWidth, 40, 13).fill();
+        ctx.roundRect(290, 185, neededWidth, 40, 25).fill();
 
-        const profilePic = await Canvas.loadImage(user.discord.user.displayAvatarURL({ format: 'png'}));
+        // draw profile picture
+        const profilePic = await Canvas.loadImage(user.discord.user.displayAvatarURL({ format: 'png', size: 300}));
         ctx.save();
         roundedImage(ctx, 70, 50, 200, 200, 23);
         ctx.stroke()
@@ -204,6 +176,7 @@ class Level extends Command {
         ctx.drawImage(profilePic, 70, 50, 200, 200);
         ctx.restore();
 
+        // draw activity circle
         let status = member.presence?.status;
         if(status === 'online') ctx.strokeStyle = '#3ba55d';
         if(status === 'idle') ctx.strokeStyle = '#faa81a';
@@ -212,6 +185,7 @@ class Level extends Command {
         ctx.lineWidth = 3.5;
         ctx.roundRect(70, 50, 200, 200, 23).stroke();
 
+        // send the canvas to discord
         const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'level-' + user.discord.user.id + '.png');
         if(interaction) interaction.editReply({files: [attachment] });
         if(message) message.reply({ files: [attachment] });
