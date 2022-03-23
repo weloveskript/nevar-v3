08c45dfbe473d5b893162a2e76856649e868e7b3
Tintenpatron <p429344@gmail.com>
2022-03-23 23:08:27 +0100

    :green_heart: op

---
+++ src/commands/staff/op.js
@@ -0,0 +1,39 @@
+const Command = require('../../core/command');
+const {SlashCommandBuilder} = require("@discordjs/builders");
+const Resolver = require('../../helper/resolver');
+
+class Op extends Command {
+    constructor(client){
+        super(client, {
+            name: "op",
+            dirname: __dirname,
+            description: "staff/op:general:description",
+            cooldown: 3000,
+            staffOnly: true,
+            slashCommand: {
+                addCommand: false
+            }
+        });
+    }
+
+    async run(interaction, message, args, data){
+        let member = interaction?.member || message?.member;
+        let channel = interaction?.channel || message?.channel;
+        let guild = interaction?.guild || message?.guild;
+        if(args[0]){
+            member = await Resolver.resolveMember(({
+                message: message,
+                search: args[0],
+            }));
+        }
+        if(!member) member = interaction?.member || message?.member
+        let userName = member.user.username;
+
+        channel.send({content: guild.translate("staff/op:main:message")
+                .replace('{user}', userName)})
+
+
+    }
+}
+
+module.exports = Op;
