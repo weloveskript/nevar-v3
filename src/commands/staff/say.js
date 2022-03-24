0dd0b9ba1b264ab667664d2b44ddd0a22616953a
Tintenpatron <p429344@gmail.com>
2022-03-24 17:42:11 +0100

    :green_heart: say

---
+++ src/commands/staff/say.js
@@ -0,0 +1,36 @@
+const Command = require('../../core/command');
+const {SlashCommandBuilder} = require("@discordjs/builders");
+const Resolver = require('../../helper/resolver');
+
+class Say extends Command {
+    constructor(client){
+        super(client, {
+            name: "say",
+            dirname: __dirname,
+            description: "staff/say:general:description",
+            cooldown: 3000,
+            staffOnly: true,
+            slashCommand: {
+                addCommand: false
+            }
+        });
+    }
+
+    async run(interaction, message, args, data){
+        let member = interaction?.member || message?.member;
+        let channel = interaction?.channel || message?.channel;
+        let guild = interaction?.guild || message?.guild;
+
+        if(!args[0]){
+            if(message) return message.send(this.client.usageEmbed(guild, this, data));
+            if(interaction) return interaction.send(this.client.usageEmbed(guild, this, data));
+        }
+
+
+        channel.send({content: args.join(' ')}).catch(() => {});
+
+
+    }
+}
+
+module.exports = Say;
