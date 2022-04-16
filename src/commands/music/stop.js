0e9766d392276366f9992db1146bf177044b3064
Tintenpatron <p429344@gmail.com>
2022-04-16 17:43:00 +0200

    :green_heart: stop

---
+++ src/commands/music/stop.js
@@ -0,0 +1,65 @@
+const Command = require('../../core/command');
+const { MessageEmbed } = require('discord.js');
+const {SlashCommandBuilder} = require("@discordjs/builders");
+const { QueueRepeatMode } = require('discord-player')
+
+class Stop extends Command {
+
+    constructor(client) {
+        super(client, {
+            name: "stop",
+            description: "music/stop:general:description",
+            dirname: __dirname,
+            cooldown: 5000,
+            slashCommand: {
+                addCommand: true,
+                data:
+                    new SlashCommandBuilder()
+            }
+        });
+    }
+
+    async run(interaction, message, args, data) {
+
+        let member = message?.member || interaction?.member;
+        let channel = message?.channel || interaction?.channel;
+        let guild = message?.guild || interaction?.guild;
+
+        const queue = this.client.player.getQueue(guild.id);
+        if(!queue || !queue.playing){
+            let embed = new MessageEmbed()
+                .setAuthor({name: this.client.user.username, iconURL: this.client.user.displayAvatarURL(), url: this.client.website})
+                .setDescription(guild.translate("music/stop:main:errors:notPlaying")
+                    .replace('{emotes.error}', this.client.emotes.error))
+                .setColor(this.client.embedColor)
+                .setFooter({text: data.guild.footer});
+            if(message) return message.send(embed);
+            if(interaction) return interaction.send(embed);
+        }
+
+        if(!member.voice?.channel || queue.connection.channel.id !== member.voice.channel.id){
+            let embed = new MessageEmbed()
+                .setAuthor({name: this.client.user.username, iconURL: this.client.user.displayAvatarURL(), url: this.client.website})
+                .setDescription(guild.translate("music/stop:main:errors:sameChannel")
+                    .replace('{emotes.error}', this.client.emotes.error))
+                .setColor(this.client.embedColor)
+                .setFooter({text: data.guild.footer});
+            if(message) return message.send(embed);
+            if(interaction) return interaction.send(embed);
+        }
+
+        queue.destroy();
+
+        let embed = new MessageEmbed()
+            .setAuthor({name: this.client.user.username, iconURL: this.client.user.displayAvatarURL(), url: this.client.website})
+            .setDescription(guild.translate("music/stop:main:stopped")
+                .replace('{emotes.success}', this.client.emotes.success))
+            .setColor(this.client.embedColor)
+            .setFooter({text: data.guild.footer});
+        if(message) return message.send(embed);
+        if(interaction) return interaction.send(embed);
+
+    }
+}
+
+module.exports = Stop;
